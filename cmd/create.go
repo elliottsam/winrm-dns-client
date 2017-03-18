d1b31381c7b75945da450307d6d22a521ece0acc
Sam Elliott <me@sam-e.co.uk>
2017-03-18 16:40:45 +0000

    Updating help for create command

--- cmd/create.go
+++ cmd/create.go
@@ -27,13 +27,10 @@ import (
 // createCmd represents the create command
 var createCmd = &cobra.Command{
 	Use:   "create",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Creates a DNS record on Windows server",
+	Long: `Creates a DNS record on Windows server
+	configured in the configuration file, all flags except
+	TTL are required.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if dnsZone == "" {
 			fmt.Println("DnsZone is a required parameter")
